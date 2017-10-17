8db9d06fe149bda32e0e36cbe0e21bd4f3b117dd
Jarek Wojciechowski <jarekwojo@gmail.com>
2017-10-17 13:08:17 -0400

    add twilio notifications

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
+let client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+
 const options = {
   uri: 'http://www.aadl.org/catalog/record/1446131',
   transform: function (body) {
@@ -9,8 +11,18 @@ const options = {
 
 rp(options)
   .then(($) => {
-    console.log($('.item-request').text());
+    const availiblity = $('.item-request').text();
+    console.log(availiblity);
+    client.messages.create({ 
+        to: process.env.MY_PHONE_NUMBER, 
+        from: process.env.TWILIO_PHONE_NUMBER, 
+        body: availiblity, 
+    }, (err, message) => { 
+        console.log(message.sid); 
+    });
   })
   .catch((err) => {
     console.log(err);
   });
+
+ 
